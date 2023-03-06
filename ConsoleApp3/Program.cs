a6f74fb33478905a5851c4d197827c7b9222754d
David Kotov <David Kotov@DESKTOP-3U9FBHN>
2023-03-06 20:25:02 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            string inputnumber = Console.ReadLine();
+
+            int inputnumber2 = Convert.ToInt32(inputnumber);
+
+            if (0 < inputnumber2)
+            { 
+   
+                for (int i = 0; i < inputnumber2; i++) 
+                {
+                    
+                    Console.WriteLine(i);
+
+
+                }
+
+                Console.WriteLine(inputnumber2);
+
+
+
+            }    
+            else
+            {
+
+                Console.Write("Ошибка");
+
+
+            }
+
+
+
+
+            
+        }
+    }
+}
